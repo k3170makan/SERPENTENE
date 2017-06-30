b03a1fb0c8668a82ffb9f38984f1877f8d80317c
k3170makan <k3170makan@gmail.com>
2017-06-30 14:15:35 +0200

    initial commit

---
+++ csp-bypass/inject-me.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<?php echo isset($_GET['inhead']) ? $_GET['inhead'] : ""; ?>
+</head>
+<title> Injection Attack </title>
+<body>
+	<form>
+		<?php echo isset($_GET['inform_1']) ? $_GET['inform_1'] : ""; ?>
+		<input type="text" name=<?php echo isset($_GET['inusername']) ? $_GET['inusername'] : "username"; ?> value="username" />
+		<?php echo isset($_GET['inform_2']) ? $_GET['inform_2'] : ""; ?>
+		<input type="hidden" name="csrf_token" value="csrf_token"/>
+		<input type='password' name="password" value="password" />
+	</form>
+</body>
+</html>
